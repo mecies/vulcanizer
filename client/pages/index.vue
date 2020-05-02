722b88b63d0880b5af5960cf87aab7377927defa
mecies <maciekhnat@gmail.com>
2020-05-02 13:58:48 +0200

    remove generated readmes and unnecessary code

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,38 +1,15 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
         vulcanizer
       </h1>
       <h2 class="subtitle">
         Our extraordinary vulcano project
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style>
 .container {
@@ -60,9 +37,5 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
