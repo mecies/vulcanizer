1b64d2918575fdb552e3a14b02214479d4f50d4b
Maciej Rychlik <rychlik.maciek.ue@gmail.com>
2020-05-24 20:59:15 +0200

    Making backend docker image work

--- vulcanizer/src/main/java/org/doublem/vulcanizer/volcano/resource/VolcanoResource.java
+++ vulcanizer/src/main/java/org/doublem/vulcanizer/volcano/resource/VolcanoResource.java
@@ -25,7 +25,7 @@ public class VolcanoResource {
     VolcanoRetrievingService volcanoRetrievingService;
 
     @Inject
-    VolcanoParsingService volcanoParsingService;
+    private VolcanoParsingService volcanoParsingService;
 
     @GET
     @Path("/source")
